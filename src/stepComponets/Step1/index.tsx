b67a12f470ac1efb97dadfb584ce871945e0a4e1
Amanda Duarte <amandaduuaarte@gmail.com>
2023-08-02 22:14:51 -0300

    refactor: Adjusting some files

--- src/stepComponets/Step1/index.tsx
+++ src/stepComponets/Step1/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import {
     Container,
+    Content,
     Title,
     Description,
     ContentInputs,
@@ -18,31 +19,33 @@ function Step1() {
     };
     return (
         <Container>
-            <Title>Personal info</Title>
-            <Description>
-                Please provide your name, email, address, and phone number.
-            </Description>
+            <Content>
+                <Title>Personal info</Title>
+                <Description>
+                    Please provide your name, email, address, and phone number.
+                </Description>
 
-            <ContentInputs>
-                <TextField
-                    control={control}
-                    name="name"
-                    label="Name"
-                    placeholder="e.g Stephen King"
-                />
-                <TextField
-                    control={control}
-                    name="email"
-                    label="Email Address"
-                    placeholder="e.g stephenKing@lorem.com"
-                />
-                <TextField
-                    control={control}
-                    name="phone"
-                    label="Phone Number"
-                    placeholder="e.g +1 234 567 890"
-                />
-            </ContentInputs>
+                <ContentInputs>
+                    <TextField
+                        control={control}
+                        name="name"
+                        label="Name"
+                        placeholder="e.g Stephen King"
+                    />
+                    <TextField
+                        control={control}
+                        name="email"
+                        label="Email Address"
+                        placeholder="e.g stephenKing@lorem.com"
+                    />
+                    <TextField
+                        control={control}
+                        name="phone"
+                        label="Phone Number"
+                        placeholder="e.g +1 234 567 890"
+                    />
+                </ContentInputs>
+            </Content>
 
             <ContentButton>
                 <Button
--- src/stepComponets/Step1/styles.ts
+++ src/stepComponets/Step1/styles.ts
@@ -1,12 +1,33 @@
 import { styled } from "styled-components";
 import colors from "../../styles/colors";
+import devices from "../../constants/devices";
 
 export const Container = styled.div`
     display: flex;
     flex-direction: column;
     padding: 32px;
-    margin-left: 32px;
-    width: 100%;
+    margin-left: 6%;
+
+    @media ${devices.md} {
+        padding: 0;
+        margin: 0;
+        background: ${colors.Alabaster};
+        align-items: center;
+    }
+`;
+
+export const Content = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 45vh;
+
+    @media ${devices.md} {
+        background: ${colors.white};
+        border: 1px solid ${colors.white};
+        border-radius: 16px;
+        height: 80%;
+        width: 100%;
+    }
 `;
 
 export const Title = styled.p`
@@ -15,6 +36,10 @@ export const Title = styled.p`
     color: ${colors.MarineBlue};
     align-self: flex-start;
     margin: 0;
+
+    @media ${devices.md} {
+        padding: 12px 32px;
+    }
 `;
 
 export const Description = styled.p`
@@ -23,13 +48,29 @@ export const Description = styled.p`
     color: ${colors.CoolGray};
     align-self: flex-start;
     margin-bottom: 24px;
+    width: 64%;
+    flex-wrap: wrap;
+
+    @media ${devices.md} {
+        padding: 0 32px;
+        width: 90%;
+    }
 `;
 
 export const ContentInputs = styled.div`
     display: flex;
     justify-content: space-between;
     flex-direction: column;
-    height: 45%;
+    height: 85%;
+    width: 80%;
+
+    @media ${devices.md} {
+        align-items: center;
+        height: 60%;
+        width: 100%;
+        margin: 0;
+        padding: 0;
+    }
 `;
 
 export const ContentButton = styled.div`
@@ -38,5 +79,14 @@ export const ContentButton = styled.div`
     align-content: flex-end;
     justify-content: right;
     width: 60%;
-    margin: 64px;
+    margin-top: 64px;
+
+    @media ${devices.md} {
+        background: ${colors.white};
+        align-items: center;
+        margin: 30px 0 0 0;
+        padding: 32px 64px 32px 32px;
+        width: 100vw;
+        align-self: flex-end;
+    }
 `;
