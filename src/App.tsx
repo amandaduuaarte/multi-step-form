90151f3579ab6be2a96d94f40052cde00452a720
amandaduuaarte <amandaduuaarte@gmail.com>
2023-08-12 13:58:05 -0300

    feat: Created useFormStep hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from "react";
 
 import Form from "./pages/Form";
 import GlobalStyle from "./styles/globalStyles";
+import { FormStepProvider } from "./hooks/useFormStep";
 
 function App() {
     return (
-        <>
+        <FormStepProvider>
             <GlobalStyle />
             <Form />
-        </>
+        </FormStepProvider>
     );
 }
 
