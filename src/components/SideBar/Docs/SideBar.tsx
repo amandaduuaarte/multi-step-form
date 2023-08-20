f5eb8e98e0642ae67f550dc41f0e9a1f8c752cbc
amandaduuaarte <amandaduuaarte@gmail.com>
2023-08-19 21:10:10 -0300

    feat: Add SideBar documentation

---
+++ src/components/SideBar/Docs/SideBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SideBar from "..";
+import { Container } from "../../../pages/Form/styles";
+
+const SideBarDocs = () => {
+    return (
+        <Container>
+            <SideBar status={1} />
+        </Container>
+    );
+};
+
+export default SideBarDocs;
